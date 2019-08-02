4cc64de622c349a54f3c57079e348638716fc98d
crian <crian84@gmail.com>
2019-08-02 15:27:31 +0200

    config: Add cwm

---
+++ .config/cwm/autostart.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+compton --config ~/.config/compton.conf &
+nitrogen --restore &
+#hsetroot -add '#161821' -add '#6b7089' -gradient -45 &
+nm-applet &
+dunst &
+~/.config/cwm/bar &
